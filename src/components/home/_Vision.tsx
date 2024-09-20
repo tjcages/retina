833bba6f5226ac57e74a713e405c027124f36ac5
tylerj <tylerjcagle@gmail.com>
2024-09-20 13:50:46 -0400

    Update _Vision.tsx

--- src/components/home/_Vision.tsx
+++ src/components/home/_Vision.tsx
@@ -6,10 +6,10 @@ const _ = () => {
       <article className="justify-center gap-12">
         <div className="col-span-full flex flex-col items-center justify-center gap-3 text-center">
           <h3>Our vision</h3>
-          <h1 className="text-pink">Every market. Onchain.</h1>
+          <h1 className="text-pink">Build on the liquidity network.</h1>
         </div>
         <div className="col-span-full flex items-center justify-center">
-          <Button size="lg">Learn more</Button>
+          <Button size="lg">Get started</Button>
         </div>
       </article>
     </section>
