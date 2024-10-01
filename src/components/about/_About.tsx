7377bb0e8a1068b4b58e2089f6e40f1dd4ab4891
Tyler J. Cagle <tylerjcagle@gmail.com>
2024-10-01 10:45:07 -0600

    small tweaks

--- src/components/about/_About.tsx
+++ src/components/about/_About.tsx
@@ -16,7 +16,7 @@ const _ = () => {
           </h2>
         </div>
         <div className="col-span-12 flex flex-col gap-3">
-          <h4>By DeFi natives. For DeFi natives.</h4>
+          <h4>For DeFi natives. By DeFi natives.</h4>
           <h4>
             The development of Unichain has been a collaborative effort across many organizations:{" "}
             <Link
@@ -26,16 +26,40 @@ const _ = () => {
             >
               Uniswap Labs
             </Link>{" "}
-            is the primary technical provider for Unichain and operates the Unichain Sequencer.
+            is the primary technical provider for Unichain and operates the Unichain Sequencer.{" "}
             <Link
               href="https://www.uniswapfoundation.org/"
               target="_blank"
               className="text-pink transition-opacity duration-200 ease-in-out hover:opacity-80"
             >
-              {" "}Uniswap Foundation
+              Uniswap Foundation
             </Link>{" "}
             leads grants and support for developers building on Unichain. Unichain was developed
-            alongside partners at Optimism, Flashbots, and Paradigm.
+            alongside partners at{" "}
+            <Link
+              href="https://optimism.io/"
+              target="_blank"
+              className="transition-opacity duration-200 ease-in-out hover:opacity-80"
+            >
+              Optimism
+            </Link>
+            ,{" "}
+            <Link
+              href="https://www.flashbots.net/"
+              target="_blank"
+              className="transition-opacity duration-200 ease-in-out hover:opacity-80"
+            >
+              Flashbots
+            </Link>
+            , and{" "}
+            <Link
+              href="https://www.paradigm.xyz/"
+              target="_blank"
+              className="transition-opacity duration-200 ease-in-out hover:opacity-80"
+            >
+              Paradigm
+            </Link>
+            .
           </h4>
         </div>
       </article>
