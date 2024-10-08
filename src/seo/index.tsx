13591abbd4e8e8a3e1a1440e760a44dc747b53a5
tylerj <tylerjcagle@gmail.com>
2024-10-08 07:40:45 -0400

    update seo

--- src/seo/index.tsx
+++ src/seo/index.tsx
@@ -2,13 +2,13 @@ import { Metadata } from "next";
 
 export const seo = {
   title: "Unichain",
-  description: "Designed for DeFi",
+  description: "Designed for DeFi. Built by Uniswap.",
   openGraph: {
     type: "website",
     locale: "en_US",
     url: "https://unichain.com",
     title: "Unichain",
-    description: "Designed for DeFi",
+    description: "Designed for DeFi. Built by Uniswap.",
     images: [
       {
         url: "https://unichain-website.vercel.app/preview.png",
