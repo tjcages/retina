204ca90ed42129caa76dd3bb99613af56deaa3d6
Tyler J. Cagle <tylerjcagle@gmail.com>
2024-10-10 08:17:38 -0400

    rm sign up banner after submission

--- src/components/shared/_SignUpForm.tsx
+++ src/components/shared/_SignUpForm.tsx
@@ -16,6 +16,7 @@ const Content = () => {
   const onFormSubmitted = () => {
     setFormSubmitted(true);
     state.isSignUpVisible = false;
+    state.showSignUpBanner = false;
     state.isSignUpSuccessVisible = true;
   };
 
