5de4cd6266468e46a6531405f14c761ed16d8b8f
Tyler J. Cagle <tylerjcagle@gmail.com>
2024-11-14 17:18:57 -0500

    add fade

--- src/app/background/page.tsx
+++ src/app/background/page.tsx
@@ -5,6 +5,7 @@ export default async function HomePage() {
     <>
       <section className="h-screen">
         <Canvas />
+        <div className="absolute inset-0 z-10 bg-gradient-to-t from-[#fef4ff] from-10% to-30%" />
       </section>
     </>
   );
