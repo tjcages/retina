1203af355db72dbf0e60ffe094da81617a17c4f8
Tyler J. Cagle <tylerjcagle@gmail.com>
2024-11-14 17:12:59 -0500

    rm logo only

--- src/app/background/page.tsx
+++ src/app/background/page.tsx
@@ -1,12 +1,10 @@
 import { Canvas } from "@/components/canvas";
-import { LogoOnly } from "@/components/home";
 
 export default async function HomePage() {
   return (
     <>
       <section className="h-screen">
         <Canvas />
-        <LogoOnly />
       </section>
     </>
   );
